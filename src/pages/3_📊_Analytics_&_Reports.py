abd48bdaf79aa4f4a0c4e4ff0cd7e57b6280b371
pecirepandrej001 <pecirepandrej38@gmail.com>
2026-08-30 02:04:12 +0200

    Delete src/pages directory

--- src/pages/3_📊_Analytics_&_Reports.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-from src.ui.components.metrics_dashboard import render_metrics
-st.title("📊 Analytics & Reports")
-if not st.session_state.get("token"):
-    st.warning("Please sign in from the main page.")
-else:
-    render_metrics()
